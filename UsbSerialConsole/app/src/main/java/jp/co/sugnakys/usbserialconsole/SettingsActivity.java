7965eea25f79ff7fb126e8875ee66a1648ca5c2c
sugnakys <sugnakys@gmail.com>
2016-08-28 10:15:00 +0900

    Add toolbar on Settings

--- UsbSerialConsole/app/src/main/java/jp/co/sugnakys/usbserialconsole/SettingsActivity.java
+++ UsbSerialConsole/app/src/main/java/jp/co/sugnakys/usbserialconsole/SettingsActivity.java
@@ -2,15 +2,21 @@ package jp.co.sugnakys.usbserialconsole;
 
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.preference.PreferenceActivity;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
 
-public class SettingsActivity extends PreferenceActivity {
+public class SettingsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_main);
+
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
+        toolbar.setTitle(getString(R.string.action_settings));
+        setSupportActionBar(toolbar);
 
         FragmentTransaction fragmentTransaction = getFragmentManager().beginTransaction();
-        fragmentTransaction.replace(android.R.id.content, new SettingsPreferenceFragment());
+        fragmentTransaction.replace(R.id.content_frame, new SettingsPreferenceFragment());
         fragmentTransaction.commit();
     }
 }
