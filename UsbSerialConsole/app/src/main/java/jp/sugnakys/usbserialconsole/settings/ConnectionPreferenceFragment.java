c5fbfbdf0fb14236db29350fd026463e8d59eadc
sugnakys <sugnakys@gmail.com>
2016-09-02 23:51:21 +0900

    Change preference from CheckBoxPreference to SwitchPreference

--- UsbSerialConsole/app/src/main/java/jp/sugnakys/usbserialconsole/settings/ConnectionPreferenceFragment.java
+++ UsbSerialConsole/app/src/main/java/jp/sugnakys/usbserialconsole/settings/ConnectionPreferenceFragment.java
@@ -1,10 +1,17 @@
 package jp.sugnakys.usbserialconsole.settings;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.ListPreference;
+import android.preference.SwitchPreference;
 
 import jp.sugnakys.usbserialconsole.R;
 
 public class ConnectionPreferenceFragment extends BasePreferenceFragment {
+
+    private ListPreference timestampFormatPref;
+    private SwitchPreference sendMessagePref;
+    private ListPreference lineFeedCodePref;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -14,5 +21,36 @@ public class ConnectionPreferenceFragment extends BasePreferenceFragment {
         listPrefKeys = new String[]{
                 getString(R.string.line_feed_code_key),
                 getString(R.string.timestamp_format_key)};
+
+        timestampFormatPref = (ListPreference) findPreference(getString(R.string.timestamp_format_key));
+        sendMessagePref = (SwitchPreference) findPreference(getString(R.string.send_message_visible_key));
+        lineFeedCodePref = (ListPreference) findPreference(getString(R.string.line_feed_code_key));
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        SharedPreferences pref = getPreferenceManager().getSharedPreferences();
+
+        timestampFormatPref.setEnabled(pref.getBoolean(getString(R.string.timestamp_visible_key), true));
+
+        boolean sendViewVisible = pref.getBoolean(getString(R.string.send_view_visible_key), true);
+        sendMessagePref.setEnabled(sendViewVisible);
+        lineFeedCodePref.setEnabled(sendViewVisible);
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        super.onSharedPreferenceChanged(sharedPreferences, key);
+
+        if (key.equals(getString(R.string.timestamp_visible_key))) {
+            SwitchPreference switchPref = (SwitchPreference) findPreference(key);
+            timestampFormatPref.setEnabled(switchPref.isChecked());
+        } else if (key.equals(getString(R.string.send_view_visible_key))) {
+            SwitchPreference switchPref = (SwitchPreference) findPreference(key);
+            sendMessagePref.setEnabled(switchPref.isChecked());
+            lineFeedCodePref.setEnabled(switchPref.isChecked());
+        }
     }
 }
