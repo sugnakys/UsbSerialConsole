037198a650e8af99b90cd232d00c866584d6f767
sugnakys <sugnakys@gmail.com>
2016-09-04 01:07:16 +0900

    Add send to function

--- UsbSerialConsole/app/src/main/java/jp/sugnakys/usbserialconsole/LogViewActivity.java
+++ UsbSerialConsole/app/src/main/java/jp/sugnakys/usbserialconsole/LogViewActivity.java
@@ -1,9 +1,13 @@
 package jp.sugnakys.usbserialconsole;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.TextView;
 
@@ -12,12 +16,14 @@ import java.io.FileInputStream;
 import java.io.IOException;
 import java.nio.charset.Charset;
 
+import jp.sugnakys.usbserialconsole.settings.SettingsActivity;
 import jp.sugnakys.usbserialconsole.util.Constants;
 
 public class LogViewActivity extends BaseAppCompatActivity {
 
     private static final String TAG = "LogViewActivity";
 
+    private File logFile;
     private Toolbar toolbar;
 
     @Override
@@ -44,11 +50,34 @@ public class LogViewActivity extends BaseAppCompatActivity {
     public void onResume() {
         super.onResume();
 
-        File logFile = (File) getIntent().getExtras().get(Constants.EXTRA_LOG_FILE);
+        logFile = (File) getIntent().getExtras().get(Constants.EXTRA_LOG_FILE);
         if (logFile != null) {
             toolbar.setTitle(logFile.getName());
             setLogText(logFile);
         }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_log_view, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent intent;
+        switch (item.getItemId()) {
+            case R.id.action_send_to:
+                intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setDataAndType(Uri.fromFile(logFile), "text/plain");
+                startActivity(intent);
+                break;
+            default:
+                Log.e(TAG, "Unknown id");
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void setLogText(File file) {
